6e1c911569e5660a783134af44c3f31d5a8ab28b
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2021-03-22 11:46:31 +0000

    add some repetition parsers

--- paars.hs
+++ paars.hs
@@ -5,7 +5,7 @@ import Control.Applicative (liftA)
 import Control.Monad (MonadPlus)
 import Data.Bifunctor (first)
 import Data.Char
-import GHC.Base
+import GHC.Base (Alternative, empty, (<|>))
 
 newtype Paars a = Paars (String -> [(a, String)])
 
@@ -14,8 +14,9 @@ instance Functor Paars where
   fmap = liftA
 
 instance Applicative Paars where
+  -- always succeeds, returning val and unmodified input
   pure :: a -> Paars a
-  pure = result
+  pure val = Paars (\inp -> [(val, inp)])
 
   (<*>) :: Paars (a -> b) -> Paars a -> Paars b
   Paars pAB <*> Paars pA = Paars (concatMap (\(f, inp') -> map (first f) (pA inp')) . pAB)
@@ -27,25 +28,26 @@ instance Monad Paars where
   return :: a -> Paars a
   return = pure
 
+  -- takes a parser of as and a function from a to a parser of bs
+  -- returns a parser of bs
+  -- the resultant parser applies func to all results of pA
   (>>=) :: Paars a -> (a -> Paars b) -> Paars b
-  (>>=) = bind
+  Paars pA >>= func = Paars (concatMap (\(v, inp') -> run (func v) inp') . pA)
 
 instance Alternative Paars where
-  empty = zero
-  (<|>) = plus
+  -- always fails
+  empty :: Paars a
+  empty = Paars (const [])
+
+  -- takes two parsers of as and returns a parser of as
+  -- the resultant parser returns results from both parsers
+  (<|>) :: Paars a -> Paars a -> Paars a
+  Paars p1 <|> Paars p2 = Paars (\inp -> p1 inp ++ p2 inp)
 
 instance MonadPlus Paars
 
 run :: Paars a -> String -> [(a, String)]
 run (Paars f) = f
-
--- always succeeds, returning val and unmodified input
-result :: a -> Paars a
-result val = Paars (\inp -> [(val, inp)])
-
--- always fails
-zero :: Paars a
-zero = Paars (const [])
 
 -- returns the first character of input
 item :: Paars Char
@@ -53,17 +55,6 @@ item = Paars item'
   where
     item' (c : cs) = [(c, cs)]
     item' _ = []
-
--- takes a parser of as and a function from a to a parser of bs
--- returns a parser of bs
--- the resultant parser applies func to all results of pA
-bind :: Paars a -> (a -> Paars b) -> Paars b
-Paars pA `bind` func = Paars (concatMap (\(v, inp') -> run (func v) inp') . pA)
-
--- takes two parsers of as and returns a parser of as
--- the resultant parser returns results from both parsers
-plus :: Paars a -> Paars a -> Paars a
-Paars p1 `plus` Paars p2 = Paars (\inp -> p1 inp ++ p2 inp)
 
 -- parses a char iff it satisfies predicate
 sat :: (Char -> Bool) -> Paars Char
@@ -88,22 +79,37 @@ letter = lower <|> upper
 alphanum :: Paars Char
 alphanum = letter <|> digit
 
-word :: Paars String
-word = [c : cs | c <- letter, cs <- word] <|> result ""
-
--- equivalent to either of:
--- neWord = do
---   c <- letter
---   cs <- word
---   return (c : cs)
--- neWord =
---   letter `bind` \c ->
---     word `bind` \cs ->
---       result (c : cs)
-
 string :: String -> Paars String
-string "" = result ""
+string "" = pure ""
 string (c : cs) = [c : cs | _ <- char c, _ <- string cs]
 
 byte :: Paars Int
 byte = [val | x <- digit, let val = digitToInt x, val < 8]
+
+-- repetition parsers
+
+-- todo: naming?
+many :: Paars a -> Paars [a]
+many pA = [x : xs | x <- pA, xs <- many pA] <|> pure []
+
+many1 :: Paars a -> Paars [a]
+many1 pA = [x : xs | x <- pA, xs <- many pA]
+
+word :: Paars String
+word = many letter
+
+nat :: Paars Int
+nat = [read xs | xs <- many1 digit]
+
+int :: Paars Int
+int = nat <|> [- n | _ <- char '-', n <- nat, n /= 0]
+
+-- int can also be defined as:
+--
+-- int = [f n | f <- operator, n <- nat, n /= 0]
+--   where
+--     operator = [negate | _ <- char '-'] <|> pure id
+--
+-- on input "-123": operator = [(negate, "123"), (id, "-123")]
+--                          n <- nat will fail on this ^
+--                          so id is not used in this case
